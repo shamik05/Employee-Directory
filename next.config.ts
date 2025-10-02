d01aa9a3aa334ca1c7fe665256a7dcc449af1656
shamik05 <64784024+shamik05@users.noreply.github.com>
2025-10-02 19:14:10 -0400

    Replace img with next/image component

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA)
   // distDir: 'build', // Changes the build output directory to `build`
+  images: {
+    remotePatterns: [new URL("https://randomuser.me/api/portraits/**")]
+  }
 }
- 
+
 export default nextConfig
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 // Row component that renders employee details given an employee object
 const Row = ({
   image, name, phone, email, dob,
 }) => {
   return (
     <tr>
-      <td><img src={image} alt="Employee thumbnail" /></td>
+      <td
+        ><Image 
+          src={image} 
+          alt="Employee thumbnail" 
+          width={100} 
+          height={100}
+          loading="lazy"
+        /></td>
       <td>{name}</td>
       <td>{phone}</td>
       <td>{email}</td>
