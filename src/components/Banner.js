5dae71462bc52a8771db68ff04b666ca5a55dd20
shamik05 <64784024+shamik05@users.noreply.github.com>
2025-10-03 17:35:04 -0400

    add subheading test to banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     <div id="banner">
       <h1>Employee Directory</h1>
-      <h2>Click on carrots to filter by heading or use the search box to narrow your results.</h2>
+      <h2>Click on table headings to filter or use the search box to narrow your results.</h2>
     </div>
   );
 }
--- src/components/Banner.test.js
+++ src/components/Banner.test.js
@@ -1,16 +1,27 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom';
-import Banner from './Banner'
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom";
+import Banner from "./Banner"
 
-describe('Component Banner', () => {
-    test('shows h1 tag', () => {
+describe("Component Banner", () => {
+    test("shows h1 tag", () => {
         render(<Banner />)
 
-        const heading = screen.getByRole('heading', {
+        const heading = screen.getByRole("heading", {
             name: "Employee Directory",
             level: 1
         })
 
         expect(heading).toBeInTheDocument();
+    })
+
+    test("shows h2 tag", () => {
+        render(<Banner />)
+
+        const subheading = screen.getByRole("heading", {
+            name: "Click on table headings to filter or use the search box to narrow your results.",
+            level: 2
+        })
+        
+        expect(subheading).toBeInTheDocument();
     })
 })
